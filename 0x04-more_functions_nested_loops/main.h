6ac1810a096e2f1034ee7ee823ff6b50e457c004
sisbeyene <sisbeyene1@gmail.com>
2022-04-15 04:23:42 +0530

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,20 @@
+/*this is th header file that includes the prototypes*/
+#ifndef MAIN_H
+#define MAIN_H
+/*this is a comment section for the math.h file that we have already written in this .h file*/
+
+char _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+
+
+#endif /* MAIN_H */
